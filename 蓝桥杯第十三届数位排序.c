bf93547ddc86d6b2f7deb2016871f2697059c90a
babulistenw <130438397+babulistenw@users.noreply.github.com>
2023-05-11 22:28:39 +0800

    Add files via upload

---
+++ 蓝桥杯第十三届数位排序.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,n,i;
+	scanf("%d %d %d",&a,&b,&n);
+	for(i=0; ;i++)
+	if(i%7>5)
+	{
+		if(i/7*(5*a+2*b)+(i%7-5)*b+5*a>=n)
+		printf("%d\n",i);
+	
+	}
+	else if(i%7<=5)
+	{
+		if(i/7*(5*a+2*b)+i%7*a>=n)
+		printf("%d\n",i);
+		
+	}
+	return 0;
+}
